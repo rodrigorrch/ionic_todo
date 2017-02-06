6c83ca9de6c0530746bee562073161fe381333ca
Renato Rodrigo Chinaglia <rodrigo.rrch@gmail.com>
2017-02-06 17:28:24 -0200

    first commit 1

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('todoapp', ['ionic'])
+var app = angular.module('todoapp', ['ionic']);
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -21,4 +21,9 @@ angular.module('todoapp', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.controller('ListaCtrl', function($scope) {
+  // auqi vem a implementacao do meu controller
+  $scope.mensagem = 'Hello world';
+});
